f622269c3ff284bffec3b44b2fc91e3eb3a90909
Himanshu Goel <himanshu@hgoel.dev>
2022-06-15 21:32:48 -0400

    Improved p2v_melt model

--- p2v_melt/generator/main.cpp
+++ p2v_melt/generator/main.cpp
@@ -12,29 +12,32 @@
 #include <stdint.h>
 #include <math.h>
 #include <vector>
+#include <algorithm>
 #include <tuple>
 
 const int grid_side = 100;
 const int grid_size = grid_side * grid_side * grid_side;
-const int sparsity = 6;
+const int sparsity = 1;
+const float surface_chain_prob = 0.4f;
+const bool surface_chains = true;
 
 const float grid_scale = 1;//1.e-9; // each voxel is 0.5nm
 
-const int NP_COUNT = 500;
-const float NP_RADIUS_UNSCALED = 2;//9.0e-9;
+const int NP_COUNT = 50;
+const float NP_RADIUS_UNSCALED = 4;//9.0e-9;
 const float NP_RADIUS = NP_RADIUS_UNSCALED / grid_scale;
 const char NP_TYPE = 1;
 const double NP_MASS = 64;//2650 * (4.0 / 3.0) * M_PI * pow(NP_RADIUS_UNSCALED, 3);
 
-const int BEAD_LENGTH_MAX = 86;
+const int BEAD_LENGTH_MAX = 24;
 const float BEAD_RADIUS = 0.5f;//0.5e-9 / grid_scale;
-const char BEAD_TYPE = 2;
+const char BEAD_TYPE_A = 2;
+const char BEAD_TYPE_B = 3;
 const double BEAD_MASS = 1;//(0.10514 * 6.02214e-23);
 
-uint8_t grid[grid_side][grid_side][grid_side];
+int grid[grid_side][grid_side][grid_side];
 
 #define grid_get(x, y, z) grid[x][y][z]
-#define grid_set(x, y, z) grid[x][y][z] = 1
 #define grid_set_zr(x, y, z, v) grid[x][y][z] = v
 
 struct particle_def_t {
@@ -55,10 +58,12 @@ struct particle_def_t {
 struct bond_def_t {
     public:
         int a, b;
+        char type;
     
-        bond_def_t(int a, int b) {
+        bond_def_t(int a, int b, char type) {
             this->a = a;
             this->b = b;
+            this->type = type;
         }
         bond_def_t(){}
 };
@@ -80,13 +85,13 @@ int add_np(int x, int y, int z, std::vector<particle_def_t> &particles, std::vec
     }
 
     int s = 2;
-    int t = 1;
+    int t = 0;
 
     // Make sure volume is empty
     for (int i = -NP_RADIUS - s; i <= NP_RADIUS + s; i++) {
         for (int j = -NP_RADIUS - s; j <= NP_RADIUS + s; j++) {
             for (int k = -NP_RADIUS - s; k <= NP_RADIUS + s; k++) {
-                if (i * i + j * j + k * k <= ceil((NP_RADIUS + 2) * (NP_RADIUS + 2))) {
+                if (i * i + j * j + k * k <= ceil((NP_RADIUS + t) * (NP_RADIUS + t))) {
                     if (x + i < 0 || x + i >= grid_side || y + j < 0 || y + j >= grid_side || z + k < 0 || z + k >= grid_side) {
                         printf ("Boundary rejection %d %d %d.\n", x+i, y+j, z+k);
                         return 0;
@@ -100,12 +105,14 @@ int add_np(int x, int y, int z, std::vector<particle_def_t> &particles, std::vec
         }
     }
 
+    int p_idx = particles.size();
+
     // Fill volume
     for (int i = -NP_RADIUS - s; i <= NP_RADIUS + s; i++) {
         for (int j = -NP_RADIUS - s; j <= NP_RADIUS + s; j++) {
             for (int k = -NP_RADIUS - s; k <= NP_RADIUS + s; k++) {
-                if (i * i + j * j + k * k <= ceil((NP_RADIUS + 2) * (NP_RADIUS + 2))) {
-                    grid_set(x + i, y + j, z + k);
+                if (i * i + j * j + k * k <= ceil((NP_RADIUS + t) * (NP_RADIUS + t))) {
+                    grid_set_zr(x + i, y + j, z + k, p_idx);
                 }
             }
         }
@@ -118,10 +125,44 @@ int add_np(int x, int y, int z, std::vector<particle_def_t> &particles, std::vec
     return 1;
 }
 
-void random_dir(int &x, int &y, int &z) {
-    x = rand() % 2 * 2 - 1;
-    y = rand() % 2 * 2 - 1;
-    z = rand() % 2 * 2 - 1;
+int bond_surface_chain(int np_idx, std::vector<particle_def_t> &particles, std::vector<bond_def_t> &bonds, std::vector<angle_def_t> &angles, std::vector<int> &bonded_chains) {
+    int x = particles[np_idx].x;
+    int y = particles[np_idx].y;
+    int z = particles[np_idx].z;
+
+    int s = 2;
+    int t = 1;
+
+    std::vector<int> bonded_chain_idxs;
+
+    // Find surface chains to bond to
+    for (int i = -NP_RADIUS - s; i <= NP_RADIUS + s; i++) {
+        for (int j = -NP_RADIUS - s; j <= NP_RADIUS + s; j++) {
+            for (int k = -NP_RADIUS - s; k <= NP_RADIUS + s; k++) {
+                if (i * i + j * j + k * k <= ceil((NP_RADIUS + t) * (NP_RADIUS + t))) {
+                    if (x + i < 0 || x + i >= grid_side || y + j < 0 || y + j >= grid_side || z + k < 0 || z + k >= grid_side)
+                        continue;
+
+                    if (grid_get(x + i, y + j, z + k)) {
+                        int tgt_idx = grid_get(x + i, y + j, z + k);
+                        if (particles[tgt_idx].type == BEAD_TYPE_A && rand() < RAND_MAX * surface_chain_prob) {
+                            bool already_bonded = std::find(bonded_chain_idxs.begin(), bonded_chain_idxs.end(), particles[tgt_idx].chain_idx) != bonded_chain_idxs.end();
+                            if (!already_bonded)
+                                already_bonded = std::find(bonded_chains.begin(), bonded_chains.end(), particles[tgt_idx].chain_idx) != bonded_chains.end();
+                            if (already_bonded)
+                                continue;
+                            bond_def_t bond(np_idx, tgt_idx, 1);
+                            bonds.push_back(bond);
+                            bonded_chain_idxs.push_back(particles[tgt_idx].chain_idx);
+                            bonded_chains.push_back(particles[tgt_idx].chain_idx);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return 1;
 }
 
 int add_free_polymer(int x, int y, int z, int chain_idx, std::vector<particle_def_t> &particles, std::vector<bond_def_t> &bonds, std::vector<angle_def_t> &angles) {
@@ -130,9 +171,9 @@ int add_free_polymer(int x, int y, int z, int chain_idx, std::vector<particle_de
     }
 
     // Place bead
-    grid_set(x, y, z);
-    particles.push_back(particle_def_t(x, y, z, BEAD_TYPE, chain_idx));
+    particles.push_back(particle_def_t(x, y, z, BEAD_TYPE_A, chain_idx));
     int prev_bead_index = particles.size() - 1;
+    grid_set_zr(x, y, z, prev_bead_index);
 
     // Place bonds
     for (int i = 0; i < BEAD_LENGTH_MAX; i++) {
@@ -165,10 +206,10 @@ int add_free_polymer(int x, int y, int z, int chain_idx, std::vector<particle_de
         int new_y = y + y_dir;
         int new_z = z + z_dir;
         
-        grid_set(new_x, new_y, new_z);
-        particles.push_back(particle_def_t(new_x, new_y, new_z, BEAD_TYPE, chain_idx));
-        bonds.push_back(bond_def_t(prev_bead_index, particles.size() - 1));
+        particles.push_back(particle_def_t(new_x, new_y, new_z, BEAD_TYPE_A, chain_idx));
+        bonds.push_back(bond_def_t(prev_bead_index, particles.size() - 1, 0));
         prev_bead_index = particles.size() - 1;
+        grid_set_zr(new_x, new_y, new_z, prev_bead_index);
 
         x = new_x;
         y = new_y;
@@ -184,7 +225,12 @@ int main(){
                 grid_set_zr(i, j, k, 0);
     std::vector<particle_def_t> particles;
     std::vector<bond_def_t> bonds;
+    std::vector<bond_def_t> surface_bonds;
     std::vector<angle_def_t> angles;
+    std::vector<int> bonded_chains;
+
+    // Add dummy particle
+    particles.push_back(particle_def_t(0, 0, 0, 0));
 
     // Set fixed seed
     srand(0);
@@ -213,15 +259,22 @@ int main(){
         }
     }
 
+    if (surface_chains) {
+        for (int i = 0; i < NP_COUNT; i++) {
+            bond_surface_chain(i+1, particles, surface_bonds, angles, bonded_chains);
+        }
+        bonds.insert(bonds.begin(), surface_bonds.begin(), surface_bonds.end());
+    }
+
     //Write the data to a LAMMPS input file
     FILE *fp = fopen("pnc_def.lammps", "w");
     fprintf(fp, "# LAMMPS data file for PNC\n");
-    fprintf(fp, "%d atoms\n", particles.size());
+    fprintf(fp, "%d atoms\n", particles.size() - 1);
     if (bonds.size() > 0) fprintf(fp, "%d bonds\n", bonds.size());
     //fprintf(fp, "%d angles\n", angles.size());
     fprintf(fp, "\n\n");
-    fprintf(fp, "%d atom types\n", 2);
-    if (bonds.size() > 0) fprintf(fp, "%d bond types\n", 1);
+    fprintf(fp, "%d atom types\n", surface_chains ? 3 : 2);
+    if (bonds.size() > 0) fprintf(fp, "%d bond types\n", 2);
     //fprintf(fp, "%d angle types\n", ANGLE_TYPE + 1);
     fprintf(fp, "\n\n");
     fprintf(fp, "%g %g xlo xhi\n", -0.01 * grid_side * grid_scale, (grid_side * 1.01) * grid_scale);
@@ -231,19 +284,25 @@ int main(){
     fprintf(fp, "Masses\n");
     fprintf(fp, "\n");
     fprintf(fp, "%d %.20g\n", NP_TYPE, NP_MASS);
-    fprintf(fp, "%d %.20g\n", BEAD_TYPE, BEAD_MASS);
+    fprintf(fp, "%d %.20g\n", BEAD_TYPE_A, BEAD_MASS);
+    if (surface_chains)
+        fprintf(fp, "%d %.20g\n", BEAD_TYPE_B, BEAD_MASS);
     fprintf(fp, "\n\n");
     fprintf(fp, "Atoms\n");
     fprintf(fp, "\n");
-    for (int i = 0; i < particles.size(); i++) {
-        fprintf(fp, "%d %d %d %g %g %g\n", i + 1, particles[i].chain_idx, particles[i].type, particles[i].x * grid_scale, particles[i].y * grid_scale, particles[i].z * grid_scale);
+    for (int i = 1; i < particles.size(); i++) {
+        bool chain_bonded = std::find(bonded_chains.begin(), bonded_chains.end(), particles[i].chain_idx) != bonded_chains.end();
+        if (chain_bonded)
+            fprintf(fp, "%d %d %d %g %g %g\n", i, particles[i].chain_idx, BEAD_TYPE_B, particles[i].x * grid_scale, particles[i].y * grid_scale, particles[i].z * grid_scale);
+        else
+            fprintf(fp, "%d %d %d %g %g %g\n", i, particles[i].chain_idx, particles[i].type, particles[i].x * grid_scale, particles[i].y * grid_scale, particles[i].z * grid_scale);
     }
     fprintf(fp, "\n\n");
     if (bonds.size() > 0){
         fprintf(fp, "Bonds\n");
         fprintf(fp, "\n");
         for (int i = 0; i < bonds.size(); i++) {
-            fprintf(fp, "%d %d %d %d\n", i + 1, 1, bonds[i].a + 1, bonds[i].b + 1);
+            fprintf(fp, "%d %d %d %d\n", i + 1, bonds[i].type + 1, bonds[i].a, bonds[i].b);
         }
         fprintf(fp, "\n\n");
     }
--- p2v_melt/setup_sizes.tcl
+++ p2v_melt/setup_sizes.tcl
@@ -1,4 +1,6 @@
 set sel [atomselect top "name 1"]
-$sel set radius 2
+$sel set radius 4
 set sel [atomselect top "name 2"]
+$sel set radius 0.5
+set sel [atomselect top "name 3"]
 $sel set radius 0.5
